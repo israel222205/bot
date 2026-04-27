d886552c651d67c89bcfecffc54ee382fca1783c
israel <141346717+israel222205@users.noreply.github.com>
2026-04-27 19:27:24 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass, flip_coin
+
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix = '$', intents=intents)
+
+#print de login----------------------
+@bot.event
+async def on_ready():
+    print(f'logado como {bot.user}')
+#------------------------------------
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hello! i am botzinho')
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f'\U0001f642')
+
+@bot.command()
+async def createpass(ctx, length = 10):
+    await ctx.send(gen_pass(length))
+    
+@bot.command()
+async def flipcoin(ctx):
+    await ctx.send(flip_coin())
+
+@bot.command()
+async def joined(ctx, *, member: discord.Member):
+    await ctx.send(f'{member} joined on {member.joined_at}')
